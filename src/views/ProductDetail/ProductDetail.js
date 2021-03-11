820ac42cd68cd34d2e2eb9010953be52e6670a1e
ducviet19 <54992665+ducviet19@users.noreply.github.com>
2021-03-11 11:09:57 +0700

    second

--- src/views/ProductDetail/ProductDetail.js
+++ src/views/ProductDetail/ProductDetail.js
@@ -1,20 +1,70 @@
 import React, { Component } from 'react';
 
-function ProductDetail () {
+function ProductDetail() {
 
     return (
 
-        <div>
-              <h1>Product Detai</h1>
-              <p>dsadasdasdsadsa</p>
+        <>
+            <div className="row">
+                <div className="col d-flex justify-content-center">
+                    <img className="w-75 h-75" src="//product.hstatic.net/1000341789/product/mausac_black_10f20shl031__1__c825731865054f5dafb26c6bcd8a3525_1024x1024.jpg" alt="Card image" />
+                </div>
+                <div className="col">
+                    <div>
+                        <h2>Áo khoác Jean nam trơn form Loose</h2>
+                    </div>
+                    <div className="status-product">
+                        <p>Trạng Thái  : Còn Hàng</p>
+                    </div>
+                    <div className="price-product">
+                        <strong>650,000đ</strong>
+                    </div>
+                    <div>
+                        <div className="row">
+                            <p className="col">Số lượng</p>
+                            <div className="col">
+                                <select>
+                                    <option >1</option>
+                                    <option >2</option>
+                                    <option >3</option>
+                                    <option >4</option>
+                                </select>
+                            </div>
 
-              <h1>Product Detai</h1>
-              <p>dsadasdasdsadsa</p>
-              <h1>Product Detai</h1>
-              <p>dsadasdasdsadsa</p>
-        </div>
+                        </div>
+                    </div>
+                    <div>
+                        <button className="btn btn-primary mr-3">Thêm vào giỏ hàng</button>
+                        <button className="btn btn-danger">Mua Ngay</button>
+                    </div>
+                    <div>
+                    </div>
+                </div>
+            </div>
 
-  
+            <div className="row">
+
+                <h2>
+                    Hình ảnh chi tiết
+                </h2>
+
+
+                <div>
+                <div>
+                <img className="w-100  h-100" src="//product.hstatic.net/1000341789/product/mausac_mindigo_10f20dja003__1__2fbe278d8d664cda9378f63e1dc5dcb4_master.jpg" alt="Card image" />
+                </div>
+                <div>
+                <img className="w-100  h-100" src="//product.hstatic.net/1000341789/product/mausac_mindigo_10f20dja003__1__2fbe278d8d664cda9378f63e1dc5dcb4_master.jpg" alt="Card image" />
+                </div>
+                <div>
+                <img className="w-100  h-100" src="//product.hstatic.net/1000341789/product/mausac_mindigo_10f20dja003__1__2fbe278d8d664cda9378f63e1dc5dcb4_master.jpg" alt="Card image" />
+                </div>
+                </div>
+               
+                
+            </div>
+        </>
+
     )
 
 };
